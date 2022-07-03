0fc0c56127540e529e583847b2c3dbbfd93a5b5b
“Mansur <mansurminnikaev@gmail.com>
2022-07-03 12:28:37 +0300

    small fixes

--- src/main/java/com/teamservice/dto/GroupDto.java
+++ src/main/java/com/teamservice/dto/GroupDto.java
@@ -1,12 +1,15 @@
 package com.teamservice.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class GroupDto {
     private Long teamleadId;
-    private String name;
     private String color;
 }
