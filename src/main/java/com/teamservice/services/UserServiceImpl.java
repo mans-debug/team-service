c0c7c20a5a44b8768940a2812bd3d56db3aa4622
“Mansur <mansurminnikaev@gmail.com>
2022-07-03 14:08:09 +0300

    removed lector_id

--- src/main/java/com/teamservice/services/UserServiceImpl.java
+++ src/main/java/com/teamservice/services/UserServiceImpl.java
@@ -3,18 +3,14 @@ package com.teamservice.services;
 import com.teamservice.dto.ExpiredUsers;
 import com.teamservice.dto.UserDto;
 import com.teamservice.models.User;
-import com.teamservice.repositories.GroupRepository;
 import com.teamservice.repositories.UserRepository;
-import jakarta.ejb.Singleton;
-import jakarta.ejb.Stateless;
-import jakarta.inject.Inject;
+
 
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
 public class UserServiceImpl implements UserService {
-    public static final long LECTOR_ID = 2321321;
     UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
@@ -60,7 +56,6 @@ public class UserServiceImpl implements UserService {
     public ExpiredUsers expiredLector() {
         List<User> users = userRepository.dayOverDue(3);
         return ExpiredUsers.builder()
-                .ownerId(LECTOR_ID)
                 .expiredUsers(users)
                 .build();
     }
